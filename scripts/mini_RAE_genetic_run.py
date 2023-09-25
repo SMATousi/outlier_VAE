da41dfc82d2914861d2d168dbaa3bfc03f0277a6
Seyed Mohamad Ali Tousi <77631699+SMATousi@users.noreply.github.com>
2023-09-25 10:55:36 -0500

    Add files via upload

---
+++ scripts/mini_RAE_genetic_run.py
@@ -0,0 +1,367 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import pygad
+import wandb
+import tensorflow.keras.backend as K
+from tensorflow.python.client import device_lib
+import os
+# print(device_lib.list_local_devices())
+
+import matplotlib.pyplot as plt
+tf.random.set_seed(0)
+np.random.seed(0)
+
+os.system("pip install pygad")
+
+
+num_samples = 10000
+num_dimensions = 30
+
+# Generate random samples
+raw_data = np.random.rand(num_samples, num_dimensions)
+
+outlier_indices_1 = [0,1,2,3,4,5,6]
+outlyin_amount_1 = 20
+outlier_indices_2 = [0,2,4,6,8,10,12]
+outlyin_amount_2 = 10
+outlier_indices_3 = [0,3,6,9,12,15,18]
+outlyin_amount_3 = 5
+outlier_indices_4 = [0,4,8,12,16,20,24]
+outlyin_amount_4 = 2
+outlier_indices_5 = [0,5,10,15,20,25,29]
+outlyin_amount_5 = 1.1
+outlier_indices_6 = [0,1,2,3,4,5,6]
+outlyin_amount_6 = [20,15,10,5,2,1.1,1.05]
+
+corrupted_data = raw_data
+
+corrupted_data[100:120, outlier_indices_1] = outlyin_amount_1
+corrupted_data[200:220, outlier_indices_2] = outlyin_amount_2
+corrupted_data[300:320, outlier_indices_3] = outlyin_amount_3
+corrupted_data[400:420, outlier_indices_4] = outlyin_amount_4
+corrupted_data[500:520, outlier_indices_5] = outlyin_amount_5
+corrupted_data[600:620, outlier_indices_6] = outlyin_amount_6
+
+for outlier_gene_index in range(corrupted_data.shape[1]+1):
+    corrupted_data[700+outlier_gene_index,:outlier_gene_index] = outlyin_amount_1
+
+normalized_arr = (corrupted_data - np.min(corrupted_data,axis = 0)) / (np.max(corrupted_data,axis = 0) - np.min(corrupted_data,axis = 0))
+
+
+latent_dim = 2
+# num_dimensions = 3
+
+# normalizing
+
+normalized_arr = (corrupted_data - np.min(corrupted_data,axis = 0)) / (np.max(corrupted_data,axis = 0) - np.min(corrupted_data,axis = 0))
+
+
+encoder_inputs = keras.Input(shape=(num_dimensions,))
+# x = layers.Conv2D(32, 3, activation="relu", strides=2, padding="same")(encoder_inputs)
+# x = layers.Conv2D(64, 3, activation="relu", strides=2, padding="same")(x)
+# x = layers.Flatten()(x)
+x = layers.Dense(20, activation="sigmoid")(encoder_inputs)
+x = layers.Dense(18, activation="sigmoid")(x)
+x = layers.Dense(16, activation="sigmoid")(x)
+encoder_output = layers.Dense(latent_dim, activation="sigmoid")(x)
+# z_mean = layers.Dense(latent_dim, name="z_mean")(x)
+# z_log_var = layers.Dense(latent_dim, name="z_log_var")(x)
+# z = Sampling()([z_mean, z_log_var])
+encoder = keras.Model(encoder_inputs, encoder_output, name="encoder")
+
+latent_inputs = keras.Input(shape=(latent_dim,))
+# x = layers.Dense(7 * 7 * 64, activation="relu")(latent_inputs)
+# x = layers.Reshape((7, 7, 64))(x)
+# x = layers.Conv2DTranspose(64, 3, activation="relu", strides=2, padding="same")(x)
+# x = layers.Conv2DTranspose(32, 3, activation="relu", strides=2, padding="same")(x)
+# decoder_outputs = layers.Conv2DTranspose(1, 3, activation="sigmoid", padding="same")(x)
+x = layers.Dense(16, activation="sigmoid")(latent_inputs)
+x = layers.Dense(18, activation="sigmoid")(x)
+x = layers.Dense(20, activation="sigmoid")(x)
+# x = layers.Dense(512, activation="relu")(x)
+decoder_outputs = layers.Dense(num_dimensions, activation="sigmoid")(x)
+decoder = keras.Model(latent_inputs, decoder_outputs, name="decoder")
+
+
+
+class RAE(keras.Model):
+    def __init__(self, encoder, decoder, **kwargs):
+        super().__init__(**kwargs)
+        self.encoder = encoder
+        self.decoder = decoder
+        self.total_loss_tracker = keras.metrics.Mean(name="total_loss")
+        self.reconstruction_loss_tracker = keras.metrics.Mean(
+            name="reconstruction_loss"
+        )
+        self.z_tracker = keras.metrics.Mean(name="z_loss")
+        self.REG_tracker = keras.metrics.Mean(name="REG_loss")
+
+    @property
+    def metrics(self):
+        return [
+            self.total_loss_tracker,
+            self.reconstruction_loss_tracker,
+            self.z_tracker,
+            self.REG_tracker,
+        ]
+
+    def train_step(self, data):
+        with tf.GradientTape(persistent=True) as tape:
+            z = self.encoder(data)
+            reconstruction = self.decoder(z)
+#             reconstruction_loss = tf.reduce_mean(
+#                 tf.reduce_sum(
+#                     keras.losses.binary_crossentropy(data, reconstruction), axis=(1, 2)
+#                 )
+#             )
+            reconstruction_loss = tf.keras.losses.MeanSquaredError(reduction=tf.keras.losses.Reduction.NONE)(data,reconstruction)
+            # reconstruction_loss = tf.keras.losses.BinaryCrossentropy(reduction=tf.keras.losses.Reduction.NONE)(data, reconstruction)
+#
+            z_loss = K.mean(K.square(z), axis=[1])
+
+
+#             gradients = tape.gradient(reconstruction, self.encoder(data))
+
+
+#             gradient_norm = 0.0
+#             for gradient in gradients:
+#                 if gradient is not None:
+#                     gradient_norm += tf.norm(gradient, ord=2)
+
+
+            REG_loss = K.mean(K.square(K.gradients(K.square(reconstruction), z)))
+
+            z_loss_w = 0.01
+            REG_loss_w = 0.01
+
+            total_loss = reconstruction_loss +  z_loss_w * z_loss + REG_loss_w * REG_loss
+            # total_loss = reconstruction_loss
+
+            grads = tape.gradient(total_loss, self.trainable_weights)
+            self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
+            self.total_loss_tracker.update_state(total_loss)
+            self.reconstruction_loss_tracker.update_state(reconstruction_loss)
+            self.z_tracker.update_state(z_loss)
+            self.REG_tracker.update_state(REG_loss)
+            del tape
+            return {
+                "loss": self.total_loss_tracker.result(),
+                "reconstruction_loss": self.reconstruction_loss_tracker.result(),
+                "z_loss": self.z_tracker.result(),
+                "REG_loss": self.REG_tracker.result(),
+            }
+
+
+# mnist_data = mat_data['X']
+tdata = np.concatenate([normalized_arr], axis=0)
+tdata = np.expand_dims(tdata, -1).astype("float32")
+
+rae = RAE(encoder, decoder)
+rae.compile(optimizer=tf.keras.optimizers.Adam())
+history = rae.fit(creditdata, epochs=100, batch_size=256)
+
+
+best_solutions = []
+
+wandb.init(
+    # set the wandb project where this run will be logged
+    project="RAE_Outlier_GA_Dual_Test_normalized",
+
+    # track hyperparameters and run metadata
+#     config={
+#     "learning_rate": 0.02,
+#     "architecture": "CNN",
+#     "dataset": "CIFAR-100",
+#     "epochs": 20,
+#     }
+)
+
+for out_indexing in range(1,normalized_arr.shape[1]+1):
+
+    def fitness_func(ga_instance, solution, solution_idx):
+
+        inliers = normalized_arr[10:13,:]
+
+        avg_ins = np.mean(inliers, axis=0)
+        avg_ins = avg_ins.reshape([1,30])
+
+        particle = normalized_arr[700+out_indexing,:]
+        particle = particle.reshape([1,30])
+
+    #     abn_subspace = solution * val_features[6728,:]
+
+    #     abn_subspace = abn_subspace.reshape([1,30])
+
+        avg_in_rec = []
+
+        for index in range(inliers.shape[0]):
+
+            candidate_inlier = inliers[index,:]
+            candidate_inlier = candidate_inlier.reshape([1,30])
+
+            in_normal_subspace = solution
+            in_bad_subspace = 1 - solution
+
+            in_remain = candidate_inlier * in_normal_subspace
+
+
+
+            in_replace = in_bad_subspace * avg_ins
+
+            in_candidate = in_remain + in_replace
+
+            z = rae.encoder(in_candidate)
+            in_candidate_rec = rae.decoder(z)
+
+
+            rec_loss = tf.keras.losses.MeanSquaredError()(in_candidate,in_candidate_rec)
+
+            avg_in_rec.append(rec_loss.numpy())
+
+        avg_in_rec = np.array(avg_in_rec)
+        avg_in_rec = np.mean(avg_in_rec)
+
+
+
+
+
+    #     z_mean, z_log_var, z = vae.encoder(particle)
+    #     reconstruction_1 = vae.decoder(z)
+
+        out_normal_subspace = solution
+        out_bad_subspace = 1 - solution
+
+        out_remain = particle * out_normal_subspace
+
+
+
+        out_replace = avg_ins * out_bad_subspace
+
+        out_candidate = out_remain + out_replace
+
+
+        z = rae.encoder(out_candidate)
+        out_candidate_rec = rae.decoder(z)
+
+        rec_loss = tf.keras.losses.MeanSquaredError()(out_candidate,out_candidate_rec)
+        rec_loss = rec_loss.numpy()
+
+        fitness = rec_loss / (avg_in_rec * 1000)
+
+        return -fitness
+
+
+
+    # def fitness_func_avg(ga_instance, solution, solution_idx):
+
+    #     fit_mean = []
+
+    #     for i in range(10):
+    #         fit_mean.append(fit_test(solution))
+
+    #     return np.mean(np.array(fit_mean))
+
+    def on_generation(ga):
+        print("Generation", ga.generations_completed)
+
+        solution, solution_fitness, solution_idx = ga_instance.best_solution()
+
+        wandb.log({"GA - " + str(out_indexing) + "/solution_fitness": solution_fitness})
+
+        print(solution_fitness)
+
+
+    fitness_function = fitness_func
+
+    num_generations = 40
+    num_parents_mating = 2
+
+    sol_per_pop = 10
+    num_genes = normalized_arr.shape[1]
+
+    init_range_low = -2
+    init_range_high = 5
+
+    parent_selection_type = "tournament"
+    K_tournament = 5
+    keep_parents = 1
+
+    space = [[0,1] for i in range(num_genes)]
+
+    crossover_type = "single_point"
+
+    mutation_type = "random"
+    mutation_percent_genes = 5
+
+    ga_instance = pygad.GA(num_generations=num_generations,
+                           num_parents_mating=num_parents_mating,
+                           fitness_func=fitness_function,
+                           sol_per_pop=sol_per_pop,
+                           num_genes=num_genes,
+                           init_range_low=init_range_low,
+                           init_range_high=init_range_high,
+                           parent_selection_type=parent_selection_type,
+                           K_tournament = K_tournament,
+                           # keep_parents=keep_parents,
+                           keep_elitism=2,
+                           crossover_type=crossover_type,
+                           mutation_type=mutation_type,
+                           mutation_percent_genes=mutation_percent_genes,
+                           on_generation=on_generation,
+                           gene_space = space)
+
+    ga_instance.run()
+
+    solution, solution_fitness, solution_idx = ga_instance.best_solution()
+    print("##########  End of the ", out_indexing, " epoch ##########")
+    print("Parameters of the best solution : {solution}".format(solution=solution))
+
+    best_solutions.append(solution)
+
+    sample = corrupted_data[700+out_indexing,:]
+
+    TP = 0
+    FP = 0
+    TN = 0
+    FN = 0
+
+    for gene_no in range(30):
+
+        if ((sample[gene_no] != 20) and (solution[gene_no] == 1)):
+
+            TP = TP + 1
+
+        elif (sample[gene_no] == 20) and (solution[gene_no] == 1):
+
+            FP = FP + 1
+
+        elif (sample[gene_no] == 20) and (solution[gene_no] == 0):
+
+            TN = TN + 1
+
+        elif (sample[gene_no] != 20) and (solution[gene_no] == 0):
+
+            FN = FN + 1
+
+    try:
+        precision = TP / (TP + FP)
+    except:
+        print("********************************** CHECK! **********************************************")
+        precision = 1
+
+    recall = TP / (TP + FN)
+
+    F1 = 2 * (precision * recall)/(precision + recall)
+
+    MCC = (TP * TN - FP * FN) / np.sqrt((TP + FP) * (TP + FN) * (TN + FP) * (TN + FN))
+
+    wandb.log({"Metrics/precision": precision,
+               "Metrics/recall": recall,
+               "Metrics/F1": F1,
+               "Metrics/MCC": MCC,
+               "Metrics/num": out_indexing})
+
+best_solutions = np.array(best_solutions)
+np.save('best_solutions.npy', best_solutions)
