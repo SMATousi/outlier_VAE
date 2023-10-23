a7f0ebeade08fa1325037ea1c0d7e7a1640d1386
S M A Tousi <mohamadali.tousi@gmail.com>
2023-10-23 15:35:00 -0500

    minor changes

--- mag_test_script.py
+++ mag_test_script.py
@@ -1,18 +1,36 @@
 from utils import *
+import wandb
 
 
+wandb.init(
+    # set the wandb project where this run will be logged
+    project="RAE_OD_MagTest", name="Magnitude Test"
+    
+    # track hyperparameters and run metadata
+#     config={
+#     "learning_rate": 0.02,
+#     "architecture": "CNN",
+#     "dataset": "CIFAR-100",
+#     "epochs": 20,
+#     }
+)
 
 ##################### Testing the Magnitude of the Outliers #######################
 
+mags = [1.5, 2, 4, 8, 16]
 
-for mag in range(1,20):
+for mag in mags:
 
     print("Starting the stage: ", mag)
 
 
     precision, recall, f1 = run_RAE(outlier_magnitude_factor = mag)
 
+    wandb.log({"Metrics/Precision": precision, "Metrics/recall": recall, "Metrics/F1": f1})
 
     
+
+
+
 
 
