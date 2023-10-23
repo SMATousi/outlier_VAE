2267156787fe4b6973b92019751c53fe0688fbcc
S M A Tousi <mohamadali.tousi@gmail.com>
2023-10-23 15:37:11 -0500

    123

--- mag_test_script.py
+++ mag_test_script.py
@@ -4,7 +4,7 @@ import wandb
 
 wandb.init(
     # set the wandb project where this run will be logged
-    project="RAE_OD_MagTest", name="Magnitude Test"
+    project="RAE_OD_MagTest", name="Magnitude Test, num_samples = 500"
     
     # track hyperparameters and run metadata
 #     config={
@@ -24,11 +24,11 @@ for mag in mags:
     print("Starting the stage: ", mag)
 
 
-    precision, recall, f1 = run_RAE(outlier_magnitude_factor = mag)
+    precision, recall, f1 = run_RAE(outlier_magnitude_factor = mag, num_samples=500)
 
     wandb.log({"Metrics/Precision": precision, "Metrics/recall": recall, "Metrics/F1": f1})
 
-    
+
 
 
 
