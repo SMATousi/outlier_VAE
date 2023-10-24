5078ab22d562cd2cef33945138b56f68c40893db
Ali Tousi <alitousi@mwc-019118.dhcp.missouri.edu>
2023-10-24 14:43:41 -0500

    123412341234123412341234123412341234123412341234

--- mag_test_script.py
+++ mag_test_script.py
@@ -4,7 +4,7 @@ import wandb
 
 wandb.init(
     # set the wandb project where this run will be logged
-    project="RAE_OD_MagTest", name="MagTest, ep=200 num_samples = 5000"
+    project="RAE_OD_MagTest", name="Wtest, ep=200 num_samples = 5000"
     
     # track hyperparameters and run metadata
 #     config={
@@ -18,15 +18,16 @@ wandb.init(
 ##################### Testing the Magnitude of the Outliers #######################
 
 mags = [1.5, 2, 4, 6, 8, 10, 12, 16]
+ws = [0.001, 0.005, 0.01, 0.05, 0.1, 0.2, 0.5]
 
-for mag in mags:
+for w in ws:
 
     print("Starting the stage: ", mag)
 
 
-    precision, recall, f1 = run_RAE(outlier_magnitude_factor = mag, epochs=200, num_samples=5000)
+    precision, recall, f1 = run_RAE(z_loss_weight=w, reg_loss_weight=w, epochs=200, num_samples=5000)
 
-    wandb.log({"Metrics/mag": mag, "Metrics/Precision": precision, "Metrics/recall": recall, "Metrics/F1": f1})
+    wandb.log({"Metrics/w": w, "Metrics/Precision": precision, "Metrics/recall": recall, "Metrics/F1": f1})
 
 
 
