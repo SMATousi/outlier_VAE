d5fcba7ffa4b9baf36a7dca19f21a71f5bdc7a99
Ali Tousi <alitousi@mwc-019118.dhcp.missouri.edu>
2023-10-24 14:57:25 -0500

    1234

--- mag_test_script.py
+++ mag_test_script.py
@@ -19,15 +19,16 @@ wandb.init(
 
 mags = [1.5, 2, 4, 6, 8, 10, 12, 16]
 ws = [0.001, 0.005, 0.01, 0.05, 0.1, 0.2, 0.5]
+dims = [5, 10, 15, 20, 25, 30]
 
-for w in ws:
+for d in dims:
 
-    print("Starting the stage: ", w)
+    print("Starting the stage: ", d)
 
 
-    precision, recall, f1 = run_RAE(z_loss_weight=w, reg_loss_weight=w, epochs=200, num_samples=5000)
+    precision, recall, f1 = run_RAE(z_loss_weight=0.1, reg_loss_weight=0.1, epochs=200, num_dimensions=d, num_samples=5000)
 
-    wandb.log({"Metrics/w": w, "Metrics/Precision": precision, "Metrics/recall": recall, "Metrics/F1": f1})
+    wandb.log({"Metrics/d": d, "Metrics/Precision": precision, "Metrics/recall": recall, "Metrics/F1": f1})
 
 
 
